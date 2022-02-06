ccaba2ef540d9250f80128d8b41b2fd22b1b44b8
Yash Lamba <yashlamba2000@gmail.com>
2022-02-06 17:31:35 +0530

    Minor fix

--- components/submit_idea.js
+++ components/submit_idea.js
@@ -5,13 +5,13 @@ import Background from "./background";
 function SubmitIdea() {
     return (
         <div className="bg-[#DDEEFD] ">
-            <div className="flex justify-center max-h-64 lg:justify-between max-w-7xl items-center mx-auto">
+            <div className="flex justify-center lg:max-h-64 lg:justify-between max-w-7xl items-center mx-auto">
                 <div className="flex-col text-center lg:text-left items-center px-4 py-8 lg:px-8">
                     {/* Content */}
                     <h1 className="font-bold text-5xl text-[#0B4870]">
                         Having a eureka moment isn’t enough!
                     </h1>
-                    <p className=" font-light text-xl text-slate-600 my-2">
+                    <p className="font-light text-xl text-slate-600 my-2">
                         Take help from experienced developers and give your idea
                         a headstart.
                     </p>
